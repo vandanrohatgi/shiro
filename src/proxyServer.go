59269c79ad2adfc86a29a0a5f179ea2161ef5722
vandanrohatgi <vandanrohatgi@gmail.com>
2023-05-02 12:15:11 +0530

    add initial live reload setup
    added docker compose and dockerfiles

--- src/proxyServer.go
+++ src/proxyServer.go
@@ -6,17 +6,16 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-
-	"github.com/vandanrohatgi/shiro_waf/testServer"
 )
 
-var webServerPort string = ":8081"
+var webServerUrl string = "https://gorest.co.in/"
 var proxyServerPort string = ":8080"
 
 func main() {
-	go testServer.Server(webServerPort)
+	//go testServer.Server(webServerPort)
 	fmt.Println("Starting Proxy...")
-	origin, err := url.Parse("http://localhost" + webServerPort)
+	//origin, err := url.Parse("http://localhost" + webServerPort)
+	origin, err := url.Parse(webServerUrl)
 	if err != nil {
 		panic(err)
 	}
