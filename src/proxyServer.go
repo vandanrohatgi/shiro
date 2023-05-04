0ebfb8c021e21b77ba8209b76201d1eee2128366
vandanrohatgi <vandanrohatgi@gmail.com>
2023-05-04 12:37:23 +0530

    remove docker file & add httpbin as origin
    removed dockerfiles for creating a web server locally on docker. will be using httpbin for testing instead since it does not implement cors

--- src/proxyServer.go
+++ src/proxyServer.go
@@ -8,13 +8,11 @@ import (
 	"net/url"
 )
 
-var webServerUrl string = "https://gorest.co.in/"
+var webServerUrl string = "https://httpbin.org/"
 var proxyServerPort string = ":8080"
 
 func main() {
-	//go testServer.Server(webServerPort)
 	fmt.Println("Starting Proxy...")
-	//origin, err := url.Parse("http://localhost" + webServerPort)
 	origin, err := url.Parse(webServerUrl)
 	if err != nil {
 		panic(err)
