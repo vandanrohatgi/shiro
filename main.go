66e6e5cd58f7cd7a7039559190bc70401de91e10
vandanrohatgi <vandanrohatgi@gmail.com>
2023-06-30 17:16:08 +0530

    Refactor flag parsing and default values

--- main.go
+++ main.go
@@ -11,21 +11,16 @@ import (
 	"github.com/charmbracelet/log"
 )
 
-var targetURL, proxyPort, path string
-var verbose, monitor bool
-var timeout int
+var targetURL = "https://httpbin.org/"
+var proxyPort = "8080"
+var path = "rules.yaml"
+var verbose = false
+var monitor = false
+var timeout = 10
 var ruleconfig RuleConfig
 
 // init performs CLI flags, reads the rule file for use and sets log level
 func init() {
-	flag.StringVar(&targetURL, "targetURL", "https://httpbin.org/", "URL to proxy")
-	flag.StringVar(&proxyPort, "proxyPort", "8080", "port to host the proxy")
-	flag.StringVar(&path, "path", "rules.yaml", "path to the rules file")
-	flag.BoolVar(&verbose, "verbose", false, "Output all type of logs")
-	flag.IntVar(&timeout, "timeout", 5, "Timeout for the proxy requests")
-	flag.BoolVar(&monitor, "monitor", false, "Monitor proxy traffic and generate regex automatically")
-	flag.Parse()
-
 	log.Info("Initialising...")
 	ruleconfig = RuleConfig{
 		Path:  path,
@@ -47,10 +42,20 @@ func init() {
 		}
 	}
 	log.Debug("Ingested Rules: ", ruleconfig.Rules)
+}
 
+func parseFlags() {
+	flag.StringVar(&targetURL, "targetURL", targetURL, "URL to proxy")
+	flag.StringVar(&proxyPort, "proxyPort", proxyPort, "port to host the proxy")
+	flag.StringVar(&path, "path", path, "path to the rules file")
+	flag.BoolVar(&verbose, "verbose", verbose, "Output all types of logs")
+	flag.IntVar(&timeout, "timeout", timeout, "Timeout for the proxy requests")
+	flag.BoolVar(&monitor, "monitor", monitor, "Monitor proxy traffic and generate rules automatically")
+	flag.Parse()
 }
 
 func main() {
+	parseFlags()
 	// Goroutine when application is run in monitoring mode.
 	// To monitor for ctrl+c (SIGINT) and writes the monitored rules to a file.
 	if monitor {
